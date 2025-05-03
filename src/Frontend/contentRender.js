fe2823c3be9afd8d9bfb55f152616ab501677518
KB Master <proofofrandomness@gmail.com>
2025-05-03 13:18:32 +0300

    WIP

--- src/Frontend/contentRender.js
+++ src/Frontend/contentRender.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Button from '@mui/material/Button';
 import Avatar from "@mui/material/Avatar";
 import { InvoiceEditor } from "./InvoiceEditor";
 
